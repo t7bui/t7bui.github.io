a8bc1cf7ca2d9e91997c0f556b7380ae6610548a
lilyxbuis.gmail.com <78515178+t7bui@users.noreply.github.com>
2023-08-21 17:37:06 -0700

    create files for dark-mode

--- script.js
+++ script.js
@@ -17,3 +17,17 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// testing darkMode
+function darkMode() {
+  let element = document.body;
+  let content = document.getElementById("DarkModetext");
+  element.className = "dark-mode";
+  content.innerText = "Dark Mode is ON";
+}
+function lightMode() {
+  let element = document.body;
+  let content = document.getElementById("DarkModetext");
+  element.className = "light-mode";
+  content.innerText = "Dark Mode is OFF";
+}
